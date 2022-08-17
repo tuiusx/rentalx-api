f2cb1097ee2458bb0ae06c733dc655b5156fc1ad
tuiusx <tuiusx@icloud.com>
2022-08-17 20:43:45 -0300

    Creating spec service

---
+++ src/modules/cars/repositories/SpecificationsRepository.ts
@@ -0,0 +1,33 @@
+import { Specification } from "../model/Specification";
+import {
+    ISpecificationsRepository,
+    ICreateSpecificationDTO,
+} from "./ISpecificationsRepository";
+
+class SpecificationsRepository implements ISpecificationsRepository {
+    private specification: Specification[];
+
+    constructor() {
+        this.specification = [];
+    }
+
+    create({ description, name }: ICreateSpecificationDTO): void {
+        const specification = new Specification();
+
+        Object.assign(specification, {
+            name,
+            description,
+            created_at: new Date(),
+        });
+        this.specification.push(specification);
+    }
+
+    findByName(name: string): Specification {
+        const specification = this.specification.find(
+            (specification) => specification.name === name
+        );
+        return specification;
+    }
+}
+
+export { SpecificationsRepository };
